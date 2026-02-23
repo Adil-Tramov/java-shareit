20ae0abfe8814c07864e0bcd3930b5ca114470ca
Adil Tramov <adiltram420@gmail.com>
2026-02-23 23:13:39 +0300

    конфликт

--- .github/workflows/wait-for-it.sh
+++ .github/workflows/wait-for-it.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# wait-for-it.sh
-
 set -e
 
 host="$1"
@@ -8,8 +6,10 @@ port="$2"
 shift 2
 cmd="$@"
 
+echo "Waiting for $host:$port..."
+
 until nc -z "$host" "$port"; do
-  >&2 echo "Waiting for $host:$port..."
+  >&2 echo "Waiting for $host:$port... (sleep 2)"
   sleep 2
 done
 
