bcee7651664a217c3a97ddaa783170c5972578ac
Adil Tramov <adiltram420@gmail.com>
2026-02-23 21:56:51 +0300

    конфликты

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -6,18 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareItServer {
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItServer.class, args);
-	}
-
-<<<<<<< HEAD:src/main/java/ru/practicum/shareit/ShareItApp.java
-=======
     public static void main(String[] args) {
-        SpringApplication.run(ShareItApp.class, args);
+        SpringApplication.run(ShareItServer.class, args);
     }
->>>>>>> adac883640e89975464113c261f241542b757645
+
 }
-=======
-}
->>>>>>> da33f33 (Sprint_16):server/src/main/java/ru/practicum/shareit/ShareItServer.java
