3e95cf7db86041572873f68e4bdd0121c3ccf58d
Adil Tramov <adiltram420@gmail.com>
2026-02-24 02:09:24 +0300

    конфликт

---
+++ tests/.github/workflows/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Docker is up"
+sleep 10
+
+./wait-for-it.sh postgres 5432 -t 60
+./wait-for-it.sh server 9090 -t 120
+./wait-for-it.sh gateway 8080 -t 60
+
+echo "All services are up! Running tests..."
