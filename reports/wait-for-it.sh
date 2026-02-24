d3bca9cf6194df76cc5fab895cd79d2eb9231fab
Adil Tramov <adiltram420@gmail.com>
2026-02-24 06:58:33 +0300

    g

--- reports/wait-for-it.sh
+++ reports/wait-for-it.sh
@@ -8,10 +8,18 @@ cmd="$@"
 
 echo "Waiting for $host:$port..."
 
-until nc -z "$host" "$port"; do
-  >&2 echo "Waiting for $host:$port... (sleep 2)"
-  sleep 2
-done
+if command -v nc &> /dev/null; then
+    until nc -z "$host" "$port"; do
+        >&2 echo "Waiting for $host:$port... (sleep 2)"
+        sleep 2
+    done
+else
+
+    until echo > /dev/tcp/"$host"/"$port" 2>/dev/null; do
+        >&2 echo "Waiting for $host:$port... (sleep 2)"
+        sleep 2
+    done
+fi
 
 >&2 echo "$host:$port is available"
 exec $cmd
