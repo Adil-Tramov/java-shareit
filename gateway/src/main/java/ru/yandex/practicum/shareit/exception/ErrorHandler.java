30b254cd4d751beec279c77a3fab71b2061e0ad9
Adil Tramov <adiltram420@gmail.com>
2026-02-25 23:20:42 +0300

    fe

--- gateway/src/main/java/ru/yandex/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/yandex/practicum/shareit/exception/ErrorHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 
 import java.util.HashMap;
