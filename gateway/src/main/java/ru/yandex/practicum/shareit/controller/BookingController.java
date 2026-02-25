562d6734902c43a99ded0aae6989589204c3de4c
Adil Tramov <adiltram420@gmail.com>
2026-02-25 14:40:21 +0300

    dfb

--- gateway/src/main/java/ru/yandex/practicum/shareit/controller/BookingController.java
+++ gateway/src/main/java/ru/yandex/practicum/shareit/controller/BookingController.java
@@ -1,4 +1,62 @@
 package ru.yandex.practicum.shareit.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.shareit.client.BookingClient;
+import ru.yandex.practicum.shareit.dto.BookingDto;
+import ru.yandex.practicum.shareit.dto.BookingState;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
 public class BookingController {
+    private final BookingClient bookingClient;
+
+    @GetMapping
+    public ResponseEntity<Object> getUserBookings(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                  @RequestParam(defaultValue = "ALL") BookingState state,
+                                                  @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                  @RequestParam(defaultValue = "10") @Positive int size) {
+        log.info("Gateway: Getting user {} bookings with state {} from {} size {}", userId, state, from, size);
+        return bookingClient.getUserBookings(userId, state, from, size);
+    }
+
+    @GetMapping("/owner")
+    public ResponseEntity<Object> getOwnerBookings(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                   @RequestParam(defaultValue = "ALL") BookingState state,
+                                                   @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                   @RequestParam(defaultValue = "10") @Positive int size) {
+        log.info("Gateway: Getting owner {} bookings with state {} from {} size {}", userId, state, from, size);
+        return bookingClient.getOwnerBookings(userId, state, from, size);
+    }
+
+    @GetMapping("/{bookingId}")
+    public ResponseEntity<Object> getBookingById(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                 @PathVariable long bookingId) {
+        log.info("Gateway: Getting booking {} for user: {}", bookingId, userId);
+        return bookingClient.getBookingById(userId, bookingId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createBooking(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                @Valid @RequestBody BookingDto bookingDto) {
+        log.info("Gateway: Creating booking {} for user: {}", bookingDto, userId);
+        return bookingClient.createBooking(userId, bookingDto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public ResponseEntity<Object> approveBooking(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                 @PathVariable long bookingId,
+                                                 @RequestParam boolean approved) {
+        log.info("Gateway: {} booking {} by user: {}", approved ? "Approving" : "Rejecting", bookingId, userId);
+        return bookingClient.approveBooking(userId, bookingId, approved);
+    }
 }
